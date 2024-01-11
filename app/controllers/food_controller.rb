6607a125600022cebac2a022b8e81840c54b0600
mathsoft29 <iwuconcept@gmail.com>
2024-01-11 17:09:40 +0100

    Implemented the  new action and the create action in food controller

--- app/controllers/food_controller.rb
+++ app/controllers/food_controller.rb
@@ -10,5 +10,24 @@ class FoodController < ApplicationController
         @food = Food.where(user: current_user).find(params[:id])
       end
     
+      def new
+        params = session_params
+        @food = params ? Food.new(params) : Food.new
+        @food.user = current_user
+      end
+    
+      def create
+        @food = current_user.foods.build(food_params)
+    
+        if @food.save
+          session.delete(:new_food) if session[:new_food]
+          redirect_to foods_path, notice: "Food #{@food.name} was added succesfully!"
+        else
+          flash[:alert] = @food.errors.full_messages.first
+          session[:new_food] = @food
+          redirect_to new_food_path
+        end
+      end
+    
       
     end
