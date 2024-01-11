4ff764e0ea7990aa26dab4e004d0f074d7264bcf
mathsoft29 <iwuconcept@gmail.com>
2024-01-11 15:13:28 +0100

    Implemented the show method in the food controller

--- app/controllers/food_controller.rb
+++ app/controllers/food_controller.rb
@@ -2,4 +2,13 @@ class FoodController < ApplicationController
     def index
         @foods = Food.where(user: current_user)
       end
+
+      def show
+        redirect_to foods_path, alert: 'Not a valid Food' and return unless /\A\d+\z/.match? params[:id]
+        redirect_to foods_path, alert: 'Food was not found' and return unless food_exists? params[:id]
+    
+        @food = Food.where(user: current_user).find(params[:id])
+      end
+    
+      
     end
