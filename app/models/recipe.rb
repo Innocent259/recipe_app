36b3d27098b3bf056bafd65f8e6399ba7f94685c
Innocent NDAHIMANA <ndahimanainnocent842@gmail.com>
2024-01-11 18:26:31 +0200

    Add recipe validations and relation

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,15 @@
 class Recipe < ApplicationRecord
+    validates :name, presence: true
+    validates :preparation_time, presence: true, numericality: true
+    validates :cooking_time, presence: true, numericality: true
+    validates :description, presence: true
+
+    belongs_to :user
+    has_many :recipe_foods, dependent: :destroy
+    has_many :foods, through: :recipe_foods
+
+    def total_price
+        recipe_foods.sum { |f| f.quantity * f.food.price }
+      end
+
 end
