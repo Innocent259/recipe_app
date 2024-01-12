925289e816e2721834a22502f53a351a4a7cd4af
Innocent NDAHIMANA <ndahimanainnocent842@gmail.com>
2024-01-12 14:53:05 +0200

    Rubocop correct

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-          has_many :foods
-          has_many :recipes
-          has_many :recipe_foods, through: :recipes 
+  has_many :foods
+  has_many :recipes
+  has_many :recipe_foods, through: :recipes
 end
